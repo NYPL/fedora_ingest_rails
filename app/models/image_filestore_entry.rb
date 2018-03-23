f4a70ebdf4404ede62d7adfd932bce1a88715d92
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-03-23 06:48:26 -0400

    Improve tests & add Travis

--- app/models/image_filestore_entry.rb
+++ app/models/image_filestore_entry.rb
@@ -1,5 +1,14 @@
 # This class talks to the MySQL database that stores the images that are in isilon.
 class ImageFilestoreEntry < ActiveRecord::Base
-  self..establish_connection("#{Rails.env}_image_filestore".to_sym)
-  self.table_name = "file_store"
+  establish_connection(:image_filestore)
+  self.table_name = 'file_store'
+
+  # This DB's columns have capital letters.
+  # ActiveRecord dislikes that. This maps it to downcased.
+  columns = %w[id file_name checksum file_id time_stamp type w h status src iid uuid cdate size quar]
+  columns.each { |column| alias_attribute(column.to_sym, column.upcase.to_sym) }
+
+  def readonly?
+    true
+  end
 end
---
+++ spec/models/ami_filestore_entry_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe AmiFilestoreEntry, type: :model do
+  it { is_expected.to belong_to(:source) }
+
+  it 'Should be readonly' do
+    expect(AmiFilestoreEntry.new).to be_readonly
+  end
+
+  it 'Should not be able to save' do
+    entry = build(:ami_filestore_entry)
+    expect(entry).to be_valid
+    expect { entry.save }.to raise_error(ActiveRecord::ReadOnlyRecord)
+  end
+end
