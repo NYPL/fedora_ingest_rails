bd934be3a167f75f23c9336176528678dfe835e9
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-03-30 16:27:58 -0400

    Fetch RELS-EXT from MMS & send it to Fedora

--- app/jobs/ingest_job.rb
+++ app/jobs/ingest_job.rb
@@ -15,6 +15,7 @@ IngestJob = Struct.new(:ingest_request_id) do
     mods   = mms_client.mods_for(@ingest_request.uuid)
     rights = mms_client.rights_for(@ingest_request.uuid)
     dublin_core = mms_client.dublin_core_for(@ingest_request.uuid)
+    rels_ext= mms_client.rels_ext_for(@ingest_request.uuid)
 
     pid = "uuid:#{@ingest_request.uuid}"
     digital_object = fedora_client.repository.find_or_initialize(pid)
@@ -29,6 +30,7 @@ IngestJob = Struct.new(:ingest_request_id) do
     fedora_client.repository.add_datastream(pid: pid, dsid: 'MODSXML', content: mods, content_type: 'text/xml', checksumType: 'MD5', dsLabel: 'MODS XML record for this object')
     fedora_client.repository.add_datastream(pid: pid, dsid: 'RIGHTS',  content: rights, content_type: 'text/xml', checksumType: 'MD5', dsLabel: 'Rights XML record for this object')
     fedora_client.repository.add_datastream(pid: pid, dsid: 'DC',      content: dublin_core, formatURI: 'http://www.openarchives.org/OAI/2.0/oai_dc/', content_type: 'text/xml', checksumType: 'MD5', dsLabel: 'DC XML record for this object')
+    fedora_client.repository.add_datastream(pid: pid, dsid: 'RELS-EXT', content: rels_ext, content_type: 'application/rdf+xml', checksumType: 'MD5', dsLabel: 'RELS-EXT XML record for this object')
     digital_object.save
     Delayed::Worker.logger.debug({ uuid: pid, message: 'done ingesting' }.to_json)
   end
