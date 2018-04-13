a5082a93aff923b7124f3e93296f1b281bab82fe
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-04-13 06:55:25 -0400

    Throw exceptions if MMS response is >= 400
    Throwing an exception will:
    
    1.  Halt execution of the delayed_job
    2.  Help us not insert an error page's HTML into Fedora
    3.  Reque the job (if deleyed_job) is setup for that.

--- spec/models/mms_client_spec.rb
+++ spec/models/mms_client_spec.rb
@@ -11,28 +11,33 @@ RSpec.describe MMSClient, type: :model do
     end
 
     it 'makes a request to get MODS' do
-      expect(HTTP).to receive(:get).with('http://example.com/exports/mods/abc-123') { HTTP }
+      expect(HTTP).to receive(:get).with('http://example.com/exports/mods/abc-123', params: {}) { double(code: 200) }
       @mms_client.mods_for('abc-123')
     end
 
     it 'makes a request to get RELS-EXT' do
-      expect(HTTP).to receive(:get).with('http://example.com/exports/rels_ext/abc-123') { HTTP }
+      expect(HTTP).to receive(:get).with('http://example.com/exports/rels_ext/abc-123', params: {}) { double(code: 200) }
       @mms_client.rels_ext_for('abc-123')
     end
 
     it 'makes a request to get Rights' do
-      expect(HTTP).to receive(:get).with('http://example.com/exports/rights/abc-123') { HTTP }
+      expect(HTTP).to receive(:get).with('http://example.com/exports/rights/abc-123', params: {}) { double(code: 200) }
       @mms_client.rights_for('abc-123')
     end
 
     it 'makes a request to get Dublin Core' do
-      expect(HTTP).to receive(:get).with('http://example.com/exports/dc/abc-123') { HTTP }
+      expect(HTTP).to receive(:get).with('http://example.com/exports/dc/abc-123', params: {}) { double(code: 200) }
       @mms_client.dublin_core_for('abc-123')
     end
 
     it "makes a request to get an Item's Captures" do
-      expect(HTTP).to receive(:get).with('http://example.com/exports/get_captures/abc-123', params: {showAll: 'true'}) { HTTP }
+      expect(HTTP).to receive(:get).with('http://example.com/exports/get_captures/abc-123', params: {showAll: 'true'}) { double(code: 200) }
       @mms_client.captures_for_item('abc-123')
+    end
+
+    it "Throws exceptions on bad requests" do
+      expect(HTTP).to receive(:get).with('http://example.com/exports/dc/abc-123', params: {}) { double(code: 500) }
+      expect { @mms_client.dublin_core_for('abc-123') }.to raise_error(Exception)
     end
   end
 end
