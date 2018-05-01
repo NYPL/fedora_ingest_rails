80d21f174655b56b06137b7d0d7e372e76dc73e0
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-05-01 15:50:02 -0400

    Remove whitespace & commented-out code

--- app/models/rels_ext_index_client.rb
+++ app/models/rels_ext_index_client.rb
@@ -7,22 +7,20 @@ class RelsExtIndexClient
     @solr = RSolr.connect url: Rails.application.secrets.rels_ext_solr_url
     @solr_params = { wt: :ruby, q: '*:*', deftype: 'lucene' }
   end
-  
+
   def post_solr_doc(uuid, solr_docs_array)
-    # old_doc = self.get_doc(uuid)
-    # remove_childless_parents(old_doc, new_doc)
     json_docs = JSON.parse(solr_docs_array)
     json_docs.each { |doc| doc['lastUpdate_dt'] = Time.now.utc.iso8601 }
     @solr.add json_docs
     @solr.commit
   end
-  
+
   def get_doc(uuid)
     @solr_params[:q] = "uuid:#{uuid}"
     resp = @solr.get 'select', :params => @solr_params
     resp["response"] if resp["response"]
   end
-  
+
   def remove_childless_parents(old_doc, new_doc)
     query = 'uuid:12345'
     @solr.delete_by_query query
