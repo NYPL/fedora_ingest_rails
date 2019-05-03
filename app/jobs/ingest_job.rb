9f95cb65a3194feef1f2ea40ac4e31cc5ba19542
emu47 <kkelly219@gmail.com>
2019-05-03 11:56:25 -0400

    [KK] Moving rels_ext logic closer together.

--- app/jobs/ingest_job.rb
+++ app/jobs/ingest_job.rb
@@ -69,16 +69,19 @@ IngestJob = Struct.new(:ingest_request_id) do
         end
       end
       
+      # Datastreams with info from the `Capture` Level
       rels_ext = mms_client.rels_ext_for(uuid)
       if rels_ext.blank?
+        # Remove it from solr as there is a good chance it is there and should not be. 
         rels_ext_index_client.remove_doc_for(uuid)
       else
+        # Write the doc to solr 
         rels_for_indexing = mms_client.full_rels_ext_solr_docs_for(uuid)
         rels_indexer_response = rels_ext_index_client.post_solr_doc(uuid, rels_for_indexing)
+        
+        # Post the datastream to the repository
+        fedora_client.repository.add_datastream(pid: pid, dsid: 'RELS-EXT', content: rels_ext, mimeType: 'application/rdf+xml', checksumType: 'MD5', dsLabel: 'RELS-EXT XML record for this object')
       end
-      
-      # Datastreams with info from the `Capture` Level
-      fedora_client.repository.add_datastream(pid: pid, dsid: 'RELS-EXT', content: rels_ext, mimeType: 'application/rdf+xml', checksumType: 'MD5', dsLabel: 'RELS-EXT XML record for this object') if rels_ext.present?
       
       digital_object.save
       Delayed::Worker.logger.info("ingested capture #{uuid}", uuid: @ingest_request.uuid)
