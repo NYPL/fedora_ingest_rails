400b701ba32aae30c9d0268427d5967c48b3acc6
Chris Baker <consultcbaker@nypl.org>
2023-11-02 09:06:09 -0700

    deploy to migrated production environment

--- provisioning/travis_ci_and_cd/ecs_deploy_new.sh
+++ provisioning/travis_ci_and_cd/ecs_deploy_new.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 # Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ] || [ "$TRAVIS_BRANCH" == "$HOT_DEPLOY_BRANCH" ]; then
-  if [ "$TRAVIS_BRANCH" == "qa" ]; then
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_QA_NEW
-    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_QA_NEW
-    CLUSTER_NAME='fedora-ingest-rails-qa'
-    WEB_APP_SERVICE_NAME='fedora-ingest-rails-web-application-qa'
-    WORKER_SERVICE_NAME='fedora-ingest-rails-worker-qa'
+  if [ "$TRAVIS_BRANCH" == "qa" ] || [ "$TRAVIS_BRANCH" == "production" ]; then
+    case "$TRAVIS_BRANCH" in
+      production)
+        export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PRODUCTION_NEW
+        export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PRODUCTION_NEW
+        CLUSTER_NAME='fedora-ingest-rails-production'
+        WEB_APP_SERVICE_NAME='fedora-ingest-rails-web-application-production'
+        WORKER_SERVICE_NAME='fedora-ingest-rails-worker-production'
+        ;;
+      qa)
+        export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_QA_NEW
+        export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_QA_NEW
+        CLUSTER_NAME='fedora-ingest-rails-qa'
+        WEB_APP_SERVICE_NAME='fedora-ingest-rails-web-application-qa'
+        WORKER_SERVICE_NAME='fedora-ingest-rails-worker-qa'
+        ;;
+    esac
 
     echo "Deploying $TRAVIS_BRANCH"
     AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws ecs update-service --cluster $CLUSTER_NAME --region us-east-1 --service $WEB_APP_SERVICE_NAME --force-new-deployment
