b4af76f47e77cb2707a2bc76c9cfd672f8db728c
Jeff Croyle <jeffreycroyle@nypl.org>
2024-02-20 11:23:46 -0500

    clean up script

--- scripts/backfill_firstindexed_dt.rb
+++ scripts/backfill_firstindexed_dt.rb
@@ -3,25 +3,11 @@
 require 'rsolr'
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 
-#class DtUpdateJob
-#  def initialize(page, per_page)
-#    @page = page
-#    @per_page = per_page
-#  end
-#
-#  def perform
-#    # Call your method with the provided arguments
-#    batch_dt_update(@page, @per_page)
-#  end
-#end
-
 ActiveRecord::Base.logger.level = 2
 
 start_time = Time.now
 
 # Solr URL
-#solr_url = 'http://solr:8983/solr/repoapi'
-#solr_url = 'http://10.225.131.109:8983/solr/repoapi'
 solr_url = 'http://10.225.132.218:8983/solr/repoapi'
 
 # Initialize Solr client
@@ -84,27 +70,3 @@ end
 end_time = Time.now
 time_elapsed = end_time - start_time
 puts "Time elapsed: #{time_elapsed} seconds"
-
-#response = solr.get('select', params: { q: '*:*', fl: 'uuid,dateIndexed_s', rows: 1000 })
-
-#response['response']['docs'].each do |doc|
-#  uuid = doc['uuid']
-#  date_indexed_dt = doc['dateIndexed_s']&.first
-#  next unless date_indexed_dt
-#  update_data = [{ uuid: uuid, dateIndexed_dt: { set: date_indexed_dt } }]
-#  update_params = { params: { commit: true }, data: update_data.to_json }
-#
-#  response = solr.update(update_params)
-#  puts response
-#end
-
-# Enqueue jobs for all page numbers of all docs -- lower priority!
-# Job
-# get document uuids in batches of 1000
-# use date Indexed from doc. If nonexistent or empty, use DateIndexed from db
-# Parse to dateIndexed dt
-# Update dt or update both s and dt if pulled from db (batches of X?)
-# If a failure, go one by one. Enqueue subjobs for failures
-## use date Indexed from doc. If nonexistent or empty, use DateIndexed from db
-# Parse to dateIndexed dt
-# Update dt or update both s and dt if pulled from db
