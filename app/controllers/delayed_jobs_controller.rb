36bbca2f6e6fb521187f76a31f127da0f68769fa
Jeff Croyle <jeffreycroyle@nypl.org>
2024-03-15 10:24:05 -0400

    improvements

--- app/controllers/delayed_jobs_controller.rb
+++ app/controllers/delayed_jobs_controller.rb
@@ -4,8 +4,7 @@ class DelayedJobsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :forbidden_action
 
   def forbidden_action
-    # Respond with 403 Forbidden status
-    head :forbidden
+    redirect_to delayed_job_path, alert: 'You are not authorized to perform this action.'
   end
 end
 
