02acc98276910fa0efd4df2dba09c04954e85048
Kristopher Kelly <kkelly219@gmail.com>
2025-06-02 13:40:56 -0400

    [DR-3652] forgot this change.

--- spec/helpers/ingest_job_helper_spec.rb
+++ spec/helpers/ingest_job_helper_spec.rb
@@ -66,6 +66,7 @@ RSpec.describe 'IngestHelper', type: :helper do
       allow(MmsClient).to receive(:new).and_return(mock_mms_client)
       allow(RepoSolrClient).to receive(:new).and_return(mock_repo_solr_client)
       allow(mock_repo_solr_client).to receive(:delete_unseen_captures_below).with("MyString", ["capture_1_uuid", "capture_2_uuid"])
+      allow(mock_repo_solr_client).to receive(:update_key_fields_for_parent_uuids)
       allow(mock_mms_client).to receive(:repo_doc_for).with(capture_1[:uuid]).and_return(capture_1).once
       allow(mock_mms_client).to receive(:repo_doc_for).with(capture_2[:uuid]).and_return(capture_2).once
       allow(ImageFilestoreEntry).to receive(:where).and_return([image_filestore_entry])
