c6508bd853296ba1188e562841673f0ee70d75f1
Kristopher Kelly <kkelly219@gmail.com>
2025-07-02 10:52:06 -0400

    [DR-3740] adding one more derivative and more notes

--- scripts/one-offs/DR-3740-precache-images/precache_images.rb
+++ scripts/one-offs/DR-3740-precache-images/precache_images.rb
@@ -15,7 +15,21 @@ require 'time'
 
 solr = RSolr.connect url: ENV['REPO_SOLR_URL']
 
-parent_uuid = ARGV[0] # Example, '79d4a650-c52e-012f-67ad-58d385a7bc34'
+
+# Collection UUIDs for our top ten collections ...
+# Wallach - 79d4a650-c52e-012f-67ad-58d385a7bc34
+# Swope - 0146e060-c530-012f-1e6f-58d385a7bc34 [1.57s average]
+# Automobiles: Manufacturers catalogues - fc161e40-c5ec-012f-c513-58d385a7bc34
+# Atlases of New York City - 2600a3f0-c5ec-012f-424e-58d385a7bc34
+# The Vinkhuijzen collection of military uniforms - 51894d20-c52f-012f-657d-58d385a7bc34
+# Cigarette cards - b50ab6f0-c52b-012f-5986-58d385a7bc34 - 150
+# Photographic views of New York City - a301da20-c52e-012f-cc55-58d385a7bc34 -98
+# The Green Book - 9ea5d5b0-1117-0132-7932-58d385a7b928 (Average before caching: 3.7s for lower derivatives. After: 
+# Billy Rose Theatre Collection photograph file - 2589a880-c52c-012f-2cb4-58d385a7bc34 - 95
+# The Buttolph collection of menus - e5114e30-c52f-012f-993c-58d385a7bc34
+# New York City Directories - f7533140-3179-0134-f53a-00505686a51c
+
+parent_uuid = ARGV[0] 
 rows_per_page = 500
 
 # Benchmark tracking
@@ -29,10 +43,10 @@ def fetch_url(url)
   res = Net::HTTP.get_response(uri)
   elapsed = Time.now - start_time
   status = res.is_a?(Net::HTTPSuccess) ? 'OK' : "#{res.code} #{res.message}"
-  puts "#{uri.path} → #{status} (#{elapsed.round(2)}s)"
+  puts "#{url} → #{status} (#{elapsed.round(2)}s)"
   elapsed
 rescue => e
-  puts "#{uri.path} → ERROR (#{e.message})"
+  puts "#{url} → ERROR (#{e.message})"
   nil
 end
 
@@ -57,7 +71,7 @@ puts "-" * 60
 
   response = solr.get 'select', params: {
     q: "parentUUID:\"#{parent_uuid}\" AND type_s:Capture",
-    fl: 'imageID_string',
+    fl: 'imageID_string,uuid',
     sort: 'sortString_sort asc',
     rows: rows_per_page,
     start: start
@@ -66,7 +80,8 @@ puts "-" * 60
   docs = response.dig('response', 'docs') || []
 
   docs.each do |doc|
-    image_id = doc['imageID_string']
+    next if doc['imageID_string'].blank?
+    image_id = doc['imageID_string'].strip
     next unless image_id
 
     puts "Processing image ID: #{image_id}"
@@ -75,8 +90,12 @@ puts "-" * 60
     # These are the sizes I think are most commonly used by DCFL, but wondering if there are others we should include.
     # Each new call naturally ups the processing time. 
     urls = [
-      "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/2/#{image_id}/full/!300,300/0/default.jpg",
-      "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/2/#{image_id}/full/!760,760/0/default.jpg"
+      "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/3/#{image_id}/full/90,/0/default.jpg",
+      "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/3/#{image_id}/full/200,/0/default.jpg",
+      "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/3/#{image_id}/full/!760,760/0/default.jpg"
+      
+      # Removing this one for now because it's just too much. But... maybe for later, if there's time?
+      # "https://#{"qa-" if Rails.env != 'production'}iiif.nypl.org/iiif/3/#{image_id}/full/full/0/default.jpg"
     ]
 
     urls.each { |url| fetch_url(url) }
