86550cce2112ff4a33c5da2c6b512843a4593ab5
Keith Bauer <bauer.keith@gmail.com>
2026-06-08 15:21:15 -0400

    Set OCR XML file path

--- app/helpers/ingest_job_helper.rb
+++ app/helpers/ingest_job_helper.rb
@@ -109,8 +109,9 @@ module IngestJobHelper
         # Get the plain text from the ocr content
         if not ocr_content.nil?
           if ocr_content.include?("<alto>")
+            capture_solr_doc['ocr_text'] = "#{ENV['OCR_SOLR_FILE_PATH']}/ocr/#{uuid}"
             capture_solr_doc['mets_alto'] = ocr_content
-            capture_solr_doc['hasOCR'] = capture_solr_doc['mets_alto'].present?
+            capture_solr_doc['hasOCR'] = true
             capture_solr_doc['captureText_ocrtext'] = Nokogiri::XML(ocr_content).xpath('//String').collect { |s| s.at('@CONTENT').text }.join(" ")
 
           elsif ocr_content.include?("</html>")
